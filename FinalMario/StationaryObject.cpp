7246d226416ab4375d66a35f6f9da642141f1e2d
ADMIN <ADMIN@DESKTOP-B4C71NS>
2023-06-21 16:50:23 +0700

    Add code into BrickBreakable.cpp

--- FinalMario/StationaryObject.cpp
+++ FinalMario/StationaryObject.cpp
@@ -1,19 +1,5 @@
 #include "StationaryObject.h"
 
-void CGlassBrick::Render()
-{
-	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ID_ANI_GLASS_BRICK)->Render(x, y);
-}
-
-void CGlassBrick::GetBoundingBox(float& l, float& t, float& r, float& b)
-{
-	l = x - BRICK_BBOX_WIDTH / 2;
-	t = y - BRICK_BBOX_HEIGHT / 2;
-	r = l + BRICK_BBOX_WIDTH;
-	b = t + BRICK_BBOX_HEIGHT;
-}
-
 
 //*********************//
 
