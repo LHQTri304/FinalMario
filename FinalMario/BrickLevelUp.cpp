bab27846e9c2a07a6b4d8f4bb8264b9b63083041
ADMIN <ADMIN@DESKTOP-B4C71NS>
2023-06-21 16:51:09 +0700

    Add code into BrickLevelUp.cpp

--- FinalMario/BrickLevelUp.cpp
+++ FinalMario/BrickLevelUp.cpp
@@ -1,1 +1,31 @@
 #include "StationaryObject.h"
+
+void CQuestBrick::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+
+	animations->Get(ID_ANI_QUEST_BRICK)->Render(x, y);
+
+	if (GetState() == QUESTBRICK_STATE_ACTIVATED)
+		animations->Get(ID_ANI_BLANK_BRICK)->Render(x, y);
+}
+
+void CQuestBrick::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - BRICK_BBOX_WIDTH / 2;
+	t = y - BRICK_BBOX_HEIGHT / 2;
+	r = l + BRICK_BBOX_WIDTH;
+	b = t + BRICK_BBOX_HEIGHT;
+}
+
+void CQuestBrick::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case QUESTBRICK_STATE_WAIT:
+		break;
+	case QUESTBRICK_STATE_ACTIVATED:
+		break;
+	}
+}
